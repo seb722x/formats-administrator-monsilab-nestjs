04a398590cf5f170828d4fd4077fb41678fd0f8a
juan sebastian Rodríguez <juan.sebas-722hotmail.com>
2023-06-28 10:33:16 -0500

    Templates working, data pushing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import {  ValidationPipe, Logger, } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+  const corsOptions: CorsOptions = {
+    origin: '*', // Opcional: reemplaza '*' con el dominio desde el que deseas permitir las solicitudes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
 
 
   await app.listen(process.env.PORT);
